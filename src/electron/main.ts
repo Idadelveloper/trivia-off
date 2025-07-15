6c914339c07972d0eabe309791b56d064da96ec4
Ida Delphine <idadelm@gmail.com>
2025-07-15 12:39:19 +0100

    initial project

---
+++ src/electron/main.ts
@@ -0,0 +1,28 @@
+import { app, ipcMain, BrowserWindow } from "electron";
+import path from 'path';
+import {ipcMainHandle, isDev} from "./util.js";
+import {getStaticData, pollResources} from "./resourceManager.js";
+import {getPreloadPath} from "./pathResolver.js";
+
+// type test = string;
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: getPreloadPath(),
+        }
+    });
+    if (isDev()) {
+        mainWindow.loadURL("http://localhost:5123")
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    }
+
+    pollResources(mainWindow);
+
+    ipcMainHandle("getStaticData", () => {
+        return getStaticData();
+    })
+
+});
+
